fd6cac8beb253529ad1b14ac4c917dabe96bf535
Nicola Amadei <nicola.amadei@gmail.com>
2018-11-11 02:45:57 +0000

    New stations

--- src/app/dashboard/DashboardComponent.jsx
+++ src/app/dashboard/DashboardComponent.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 
 import TabBar from '../common/tab-bar/tab-bar'
+import SvgMap from '../common/svg-map/svg-map'
 import MapView from '../common/map-view/map-view'
 import LineListview from '../common/line-listview/line-listview'
 import './Dashboard.scss';
@@ -16,7 +17,7 @@ const DashboardComponent = ({ focusedTab, onTabChange }) => {
         <div className="Dashboard">
             <Grid container style={{ height: '100%', overflowY:'auto' }}>
                 <Grid item xs={8}>
-                    <MapView stations={stations}/>
+                    <SvgMap/>
                 </Grid>
                 <Grid item xs={4} >
                     <TabBar onTabChange={onTabChange} focusedTab={focusedTab} />
