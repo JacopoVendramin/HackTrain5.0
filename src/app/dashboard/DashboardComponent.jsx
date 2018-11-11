e26be4feda907ab8a283cf93e23269616d4cb734
Ayoub Aabass <aabass.ayoub@gmail.com>
2018-11-11 06:23:51 +0100

    ➕ Right pane head layout

--- src/app/dashboard/DashboardComponent.jsx
+++ src/app/dashboard/DashboardComponent.jsx
@@ -9,6 +9,43 @@ import TabBar from '../common/tab-bar/tab-bar'
 import LineListview from '../common/line-listview/line-listview'
 import './Dashboard.scss';
 
+const DetailCard = ({ name, status, delay }) => {
+    return (
+        <div className="DetailCard">
+            <Typography variant="h2">{name}</Typography>
+            <div>
+                <div className="DetailCard__status">
+                    <div className="DetailCard__status__label"/>
+                    <Typography variant="body1">{status}</Typography>
+                    <Typography variant="body1">•</Typography>
+                    <Typography variant="body1">{delay}s</Typography>
+                </div>
+            </div>
+
+            <div className="DetailCard__info">
+                <Grid container spacing={16}>
+                    <Grid item xs={4}>
+                        <div className="DetailCard__info__box">
+                            <Typography variant="body1"><strong>Today avarage</strong></Typography>
+                            <Typography variant="h3">0:26</Typography>
+                            <Typography variant="body1">-0.04</Typography>
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <div className="DetailCard__info__box">
+                            <Typography variant="body1"><strong>Latest dwell</strong></Typography>
+                            <Typography variant="h3">0:38</Typography>
+                            <Typography variant="body1">+0.08</Typography>
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
+    )
+}
+
 const DashboardComponent = ({ focusedTab, onTabChange }) => {
     return (
         <div className="Dashboard">
@@ -20,26 +57,28 @@ const DashboardComponent = ({ focusedTab, onTabChange }) => {
                         </header>
                     </div>
                     <LineListview items={[
-                        { name: 'London', status: 'Warning', dwellDelay: 5 },
-                        { name: 'London', status: 'Blocked', dwellDelay: 60 },
-                        { name: 'London', status: 'Warning', dwellDelay: 20 },
-                        { name: 'London', status: 'Safe', dwellDelay: 2 },
-                        { name: 'London', status: 'Warning', dwellDelay: 5 },
-                        { name: 'London', status: 'Blocked', dwellDelay: 60 },
-                        { name: 'London', status: 'Warning', dwellDelay: 20 },
-                        { name: 'London', status: 'Safe', dwellDelay: 2 },
-                        { name: 'London', status: 'Warning', dwellDelay: 5 },
-                        { name: 'London', status: 'Blocked', dwellDelay: 60 },
-                        { name: 'London', status: 'Warning', dwellDelay: 20 },
-                        { name: 'London', status: 'Safe', dwellDelay: 2 },
-                    ]} />
+                            { name: 'London', status: 'Warning', dwellDelay: 5 },
+                            { name: 'London', status: 'Blocked', dwellDelay: 60 },
+                            { name: 'London', status: 'Warning', dwellDelay: 20 },
+                            { name: 'London', status: 'Safe', dwellDelay: 2 },
+                            { name: 'London', status: 'Warning', dwellDelay: 5 },
+                            { name: 'London', status: 'Blocked', dwellDelay: 60 },
+                            { name: 'London', status: 'Warning', dwellDelay: 20 },
+                            { name: 'London', status: 'Safe', dwellDelay: 2 },
+                            { name: 'London', status: 'Warning', dwellDelay: 5 },
+                            { name: 'London', status: 'Blocked', dwellDelay: 60 },
+                            { name: 'London', status: 'Warning', dwellDelay: 20 },
+                            { name: 'London', status: 'Safe', dwellDelay: 2 },
+                        ]} />
+                    </Grid>
+                    <Grid item xs={4} >
+                        <div className="Dashboard__right">
+                            <DetailCard name='London' status='Warning' delay={5} />
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={4} >
-                    <TabBar onTabChange={onTabChange} focusedTab={focusedTab} />
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+            </div>
+        )
+    }
 
-export default DashboardComponent;
+    export default DashboardComponent;
